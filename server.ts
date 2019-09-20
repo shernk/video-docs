e48b08702ae82384b2f877b1dc0e6173b8acda5e
shernk <31926953+shernk@users.noreply.github.com>
2019-09-21 04:58:41 +0700

    set app gets environment on server.ts

--- server.ts
+++ server.ts
@@ -15,46 +15,58 @@
  * import for `ngExpressEngine`.
  */
 
-import 'zone.js/dist/zone-node';
+import "zone.js/dist/zone-node";
 
-import * as express from 'express';
-import {join} from 'path';
+import * as express from "express";
+import { join } from "path";
 
 // Express server
 const app = express();
-
+const ISPRODUCTION = process.env.isProduction ? "https://video-docs-services.herokuapp.com/api/" : false;
 const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+const DIST_FOLDER = join(process.cwd(), "dist/browser");
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+const {
+  AppServerModuleNgFactory,
+  LAZY_MODULE_MAP,
+  ngExpressEngine,
+  provideModuleMap
+} = require("./dist/server/main");
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+app.engine(
+  "html",
+  ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [provideModuleMap(LAZY_MODULE_MAP)]
+  })
+);
 
-app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
+app.set("view engine", "html");
+app.set("views", DIST_FOLDER);
 
 // Example Express Rest API endpoints
 // app.get('/api/**', (req, res) => { });
 // Serve static files from /browser
-app.get('*.*', express.static(DIST_FOLDER, {
-  maxAge: '1y'
-}));
+app.get(
+  "*.*",
+  express.static(DIST_FOLDER, {
+    maxAge: "1y"
+  })
+);
+
+// Set up environment for heroku
+app.get("/api/environment", (req, res) => {
+  res.send(ISPRODUCTION);
+});
 
 // All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
+app.get("*", (req, res) => {
+  res.render("index", { req });
 });
 
 // Start up the Node server
 app.listen(PORT, () => {
   console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
-
-
--- src/app/components/shared/services/http-utility/http-utility.service.ts
+++ src/app/components/shared/services/http-utility/http-utility.service.ts
@@ -9,16 +9,24 @@ export class HttpUtilityService extends AHttpUtilityService {
     this.updateBase();
   }
 
-  private updateBase(): string {
-    const { production: isProduction } = environment;
+  private async updateBase() {
+    // const { production: isProduction } = environment;
+    let isProduction = false;
+    try {
+      const res = await fetch("/api/environment");
+      const json = await res.json();
+      isProduction = json;
+    } catch {
+      console.error("API ERROR");
+    }
 
     const productionBase = "https://video-docs-services.herokuapp.com/api/";
 
     /***
-    ** Eps31
-    ** Time: 50:00
-    * ! Failed to get http://localhost:3000/v1/category, status 404
-    */
+     ** Eps31
+     ** Time: 50:00
+     * ! Failed to get http://localhost:3000/v1/category, status 404
+     */
 
     return isProduction ? productionBase : "http://localhost:3000/api/";
   }
