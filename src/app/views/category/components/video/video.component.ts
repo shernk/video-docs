6ec2493ce45daaa74459f47f295c629809d15213
shernk <31926953+shernk@users.noreply.github.com>
2019-09-21 11:14:12 +0700

    Modified video component

--- src/app/views/category/components/video/video.component.ts
+++ src/app/views/category/components/video/video.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-video',
-  templateUrl: './video.component.html',
-  styleUrls: ['./video.component.scss']
+  selector: "app-video",
+  templateUrl: "./video.component.html",
+  styleUrls: ["./video.component.scss"]
 })
+export class VideoComponent {
+  public hasBeenPlayed = false;
+  public videoUrl: SafeResourceUrl = "";
 
-export class VideoComponent implements OnInit {
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) {}
 
-  ngOnInit() { }
+  public playVideo() {
+    if (!this.hasBeenPlayed) {
+      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        "https://www.youtube.com/embed/gaYWg9967ZY?autoplay=1"
+      );
+    }
+
+    this.hasBeenPlayed = true;
+  }
 }
