0e0c3bf81806bf395605898442676f9c9c980125
shernk <31926953+shernk@users.noreply.github.com>
2019-09-30 18:13:19 +0700

    added meta tag to contact page

--- src/app/views/contact/contact.component.ts
+++ src/app/views/contact/contact.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { MetaTag } from "src/app/components/shared/models/enums/meta-tag.enum";
+import { Component } from "@angular/core";
+import { ASeoService } from "src/app/components/shared/seo/aseo.service";
 
 @Component({
   selector: "app-contact",
   templateUrl: "./contact.component.html",
   styleUrls: ["./contact.component.scss"]
 })
-export class ContactComponent implements OnInit {
-  constructor() {}
+export class ContactComponent {
+  constructor(public seoService: ASeoService) {
+    this.updateMetaDescription();
+  }
 
-  ngOnInit() {}
+  private updateMetaDescription(): void {
+    this.seoService.addMetaTag(MetaTag.Description, "Connect");
+  }
 }
