fc694aea724ab99c6b18b19e4e0aaf27985623f1
shernk <31926953+shernk@users.noreply.github.com>
2019-10-01 07:44:05 +0700

    added twitter meta to seo service

--- src/app/components/shared/seo/aseo.service.ts
+++ src/app/components/shared/seo/aseo.service.ts
@@ -1,4 +1,4 @@
 export abstract class ASeoService {
   public abstract addMetaTag(tagName: string, content: string): void;
-  public abstract addTwitterMeta(content: string, imageUrl: string);
+  public abstract addTwitterMeta(content: string, imageUrl: string): void;
 }
--- src/app/components/shared/seo/mock-seo.service.ts
+++ src/app/components/shared/seo/mock-seo.service.ts
@@ -1,11 +1,17 @@
 import { ASeoService } from "./aseo.service";
 
 export class MockSeoService implements ASeoService {
+  public lastImageUrl = "";
   public lastTagName = "";
   public lastContent = "";
 
   public addMetaTag(tagName: string, content: string): void {
     this.lastTagName = tagName;
     this.lastContent = content;
+  }
+
+  public addTwitterMeta(content: string, imageUrl: string): void {
+    this.lastContent = content;
+    this.lastImageUrl = imageUrl;
   }
 }
