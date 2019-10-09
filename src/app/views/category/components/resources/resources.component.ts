1448db3fe9fc3e07b08117795ab7099a86d95e52
shernk <31926953+shernk@users.noreply.github.com>
2019-10-09 15:28:28 +0700

    modified resources

--- src/app/views/category/components/resources/resources.component.ts
+++ src/app/views/category/components/resources/resources.component.ts
@@ -1,14 +1,11 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-resources",
   templateUrl: "./resources.component.html",
   styleUrls: ["./resources.component.scss"]
 })
-
-export class ResourcesComponent implements OnInit {
-  @Input() public title = '';
+export class ResourcesComponent {
+  @Input() public title = "";
   @Input() public list: string[];
-
-  public ngOnInit() {}
 }
